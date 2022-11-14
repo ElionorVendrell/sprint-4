c36861d28670faf7b70b8ce58d9bd84c102123ab
ElionorVendrell <elivendrell@gmail.com>
2022-11-15 00:14:14 +0100

    ej.3

--- src/films.js
+++ src/films.js
@@ -1,46 +1,41 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  const result = array.map((x) => x.director);
+  console.log('EXERCICE 1 ->', result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = [];
+  result = array.filter((e) => e.director === director);
+  console.log('EXERCICE 2 ->', result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  const newArray = array.filter((e) => e.director === director);
+  let long = newArray.length;
+  const suma = newArray.reduce((a, b) => a + b.score, 0);
+  let result = suma / long;
+  return result;
 }
 
-// Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
-  
-}
+// Exercise 4:  Alphabetic order by title
+function orderAlphabetically(array) {}
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
-}
+function orderByYear() {}
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
-}
+function moviesAverageByCategory() {}
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-
-}
+function hoursToMinutes() {}
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
-}
-
-
+function bestFilmOfYear() {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -53,6 +48,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     moviesAverageByCategory,
     hoursToMinutes,
-    bestFilmOfYear,
+    bestFilmOfYear
   };
 }
